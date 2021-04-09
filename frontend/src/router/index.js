9e9c349c2349491299ff51fa8b0b62531ed978e7
Anna-10 <annabohun83@gmail.com>
2021-04-09 12:19:54 +0200

    implement basic routes and auth

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,100 @@
-import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Login from '@/components/Login';
+import Register from '@/components/Register';
+import Dashboard from '@/components/Dashboard';
+import NewProduct from '@/components/NewProduct';
+import Report from '@/components/Report';
+import Admin from '@/components/Admin';
 
-Vue.use(Router);
-
-export default new Router({
+const router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld,
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        guest: true,
+      },
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+      meta: {
+        guest: true,
+      },
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: Admin,
+      meta: {
+        requiresAuth: true,
+        is_admin: true,
+      },
+    },
+    {
+      path: '/newproduct',
+      name: 'newproduct',
+      component: NewProduct,
+      meta: {
+        requiresAuth: true,
+      },
+    },
+    {
+      path: '/report',
+      name: 'report',
+      component: Report,
+      meta: {
+        requiresAuth: true,
+      },
+    },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem('jwt') == null) {
+      next({
+        path: '/login',
+        params: { nextUrl: to.fullPath },
+      });
+    } else {
+      const user = JSON.parse(localStorage.getItem('user'));
+      if (to.matched.some(record => record.meta.is_admin)) {
+        if (user.is_admin === 1) {
+          next();
+        } else {
+          next({ name: 'dashboard' });
+        }
+      } else {
+        next();
+      }
+    }
+  } else if (to.matched.some(record => record.meta.guest)) {
+    if (localStorage.getItem('jwt') == null) {
+      next();
+    } else {
+      next({ name: 'dashboard' });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
