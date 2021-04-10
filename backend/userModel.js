644e2570e40b80b3038945465e9890e4548d5069
Nazariy Bohun <nazariy.bohun@daimler.com>
2021-04-10 09:39:26 +0200

    add auth mechanism

---
+++ backend/userModel.js
@@ -0,0 +1,34 @@
+//userModel.js
+var mongoose = require('mongoose');
+//schema
+var userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+    },
+    admin: {
+        type: Boolean,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+// Export User Model
+var User = module.exports = mongoose.model('user', userSchema);
+
+
+module.exports.findByEmail = function (mail) {
+    User.find({ email: mail }).limit(1);
+}
+
+module.exports.get = function (callback, limit) {
+    User.find(callback).limit(limit);
+}
